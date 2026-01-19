dcd4ef2991bbc6093819a33a7414b53acd369055
durgeshraihero-commits <durgeshraihero@gmail.com>
2026-01-19 22:54:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -156,7 +156,7 @@ SEARCH_COMMANDS = {
         "name": "👨‍👩‍👧‍👦 Family Info",
         "type": "family_group",
         "commands": {
-            0: "/family"
+            0: "/familyinfo"
         }
     },
     "aadhar": {
