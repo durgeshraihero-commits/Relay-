4f2cd3f75d3c73775f073245db7bfce3972c361a
durgeshraihero-commits <durgeshraihero@gmail.com>
2026-02-10 16:12:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -256,7 +256,7 @@ GROUP_PRIORITIES = {
     },
     "secondary": {
         "name": "🌐 IntelX Network",
-        "identifier": "IntelXGroup",
+        "identifier": "EncoreXgroup",
         "timeout": 35,
         "weight": 7,
         "enabled": True,
